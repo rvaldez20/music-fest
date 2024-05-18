bd25bd48e918283d7b12a2ac6b396d8765ca1f92
rvaldez20 <rvaldez20@gmail.com>
2024-05-17 22:58:02 -0600

    [ADD] config gulp for compile sass, config watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
-export function hola( done ) {
-   console.log('Hola desde Gulpfile.js')
+import { src, dest, watch } from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+const sass = gulpSass(dartSass);
+
+export function css( done ) {
+   src('src/scss/app.scss')
+      .pipe( sass() )
+      .pipe( dest('build/css') )
 
    done()
+}
+
+export function dev() {
+   watch('src/scss/app.scss', css)
 }
