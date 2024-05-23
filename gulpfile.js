f318a70be31b588fc7e404391242d7d7ea6392da
rvaldez20 <rvaldez20@gmail.com>
2024-05-22 20:06:18 -0600

    [ADD] enable sourcemaps of sass

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,9 @@ import gulpSass from 'gulp-sass'
 const sass = gulpSass(dartSass);
 
 export function css( done ) {
-   src('src/scss/app.scss')
-      .pipe( sass().on('error', sass.logError) )
-      .pipe( dest('build/css') )
+   src('src/scss/app.scss', {sourcemaps: true})
+      .pipe(sass().on('error', sass.logError))
+      .pipe(dest('build/css', {sourcemaps: "."}))
 
    done()
 }
