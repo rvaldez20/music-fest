4da30e6307cbb255d0141b19ba5a976fd6a0dd5a
rvaldez20 <rvaldez20@gmail.com>
2024-05-23 13:55:03 -0600

    [ADD] with js do it efect smooth when clic in navlinks

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function(){
    navegacionFija()
    crearGaleria()
    resaltarEnlace()
+   scrollNav()
 })
 
 
@@ -105,5 +106,21 @@ function resaltarEnlace() {
          }
       })
 
+   })
+}
+
+function scrollNav() {
+   const navLinks = document.querySelectorAll('.navegacion-principal a')
+
+   navLinks.forEach(link => {
+      link.addEventListener('click', e => {
+         e.preventDefault()
+         const sectionScroll = e.target.getAttribute('href')
+         const section = document.querySelector(sectionScroll)
+
+         section.scrollIntoView({behavior: 'smooth'})
+
+         console.log(section)
+      })
    })
 }
