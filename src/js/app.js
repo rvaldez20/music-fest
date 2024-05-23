c92c42dccea9673abd094e3380cfa55e67f6f816
rvaldez20 <rvaldez20@gmail.com>
2024-05-22 21:07:14 -0600

    [ADD] finish to show grid with images in diferente media querys

--- src/js/app.js
+++ src/js/app.js
@@ -13,8 +13,40 @@ function crearGaleria() {
       imagen.src = `src/img/gallery/full/${i}.jpg`
       imagen.alt = 'Imagen Galeria'
 
-      galeria.appendChild(imagen);
+      // event handlerc (click)
+      imagen.onclick = function(e) {
+         mostrarImage(i);
+      }
 
-      console.log(imagen)
+      galeria.appendChild(imagen);
    }
+
+   function mostrarImage(index) {
+      // construir overlay que muestra la imagen mas grande
+      //! crear modal
+      const modal = document.createElement('DIV')
+      modal.classList.add('modal')
+      modal.onclick = cerrarModal
+
+      //! agregar modal al HTML
+      const body = document.querySelector('body')
+      body.appendChild(modal);
+
+      console.log(modal)
+   }
+
+   function cerrarModal() {
+      const modal = document.querySelector('.modal')
+      modal?.remove()
+   }
+
+
+
+
+
+
+   // galeria.addEventListener('click', function(e) {
+   //    const imgSelect = e.target.src.split('/')[7]
+   //    console.log(imgSelect)
+   // })
 }
